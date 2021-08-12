79b68de2b62a85944c60f98212c51c110d55361a
Daniel <82422424+Bhimmo@users.noreply.github.com>
2021-08-12 18:15:54 -0300

    Add files via upload

---
+++ components/home/Home.vue
@@ -0,0 +1,117 @@
+<template>
+  <div>
+    <h1 class="centralizado">{{ titulo }}</h1>
+    <p v-show="mensagem" class="centralizado"> {{mensagem}} </p>
+
+    <input type="search" class="filtro" 
+    @input="filtro = $event.target.value" 
+    placeholder="Filtre pelo titulo">
+
+    <ul class="lista-fotos">
+      <li class="lista-fotos-itens" v-for="foto of fotosComFiltro" :key="foto.id">
+        
+        <meu-painel :titulo="foto.titulo">
+
+          <imagem-responsiva 
+          v-meu-transform:scale.animate="1.1" 
+          :url="foto.url" 
+          :titulo="foto.titulo"/>
+
+          <router-link :to="{ name: 'alterar', params: {id:foto._id}}"> <meu-botao tipo="button" rotulo="ALTERAR" /> </router-link>
+
+          <meu-botao 
+          tipo="button" 
+          rotulo="REMOVER" 
+          @botaoAtivado="remove(foto)"
+          :confirmacao="true"
+          estilo="perigo"/>
+
+        </meu-painel>
+
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+import imagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
+import Painel from '../shared/painel/Painel.vue';
+import Botao from '../shared/botao/Botao.vue';
+import FotoService from '../../domain/foto/FotoService';
+
+export default {
+
+  components: {
+    'meu-painel': Painel,
+    'imagem-responsiva': imagemResponsiva,
+    'meu-botao': Botao,
+  },
+
+  data() {
+    return{
+      titulo: 'PicLearning',
+      fotos: [],
+      filtro: '',
+
+    }
+  },
+
+  computed: {
+    fotosComFiltro() {
+      if(this.filtro){
+        let exp = new RegExp(this.filtro.trim(), 'i');
+        return this.fotos.filter(foto => exp.test(foto.titulo));
+      }else{
+        return this.fotos;
+      }
+    }
+  },
+
+  methods: {
+
+    remove(foto) {
+
+      this.service.apaga(foto._id)
+        .then( ()=> {
+          let indice = this.fotos.indexOf(foto);
+          this.fotos.splice(indice, 1);
+          this.mensagem = 'Foto removida com sucesso';
+        }, err =>{
+          this.mensagem = 'Não foi possível remover a foto';
+        });  
+    }
+  },
+
+  created() {
+
+    this.service = new FotoService(this.$resource);
+
+    this.service
+      .lista()
+      .then(fotos => this.fotos = fotos, err => {
+        this.mensagem = err.menssage;
+      })
+  }
+}
+</script>
+
+<style>
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .lista-fotos {
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-itens {
+    display: inline-block;
+  }
+
+  .filtro {
+    display: block;
+    width: 100%;
+  }
+</style>
